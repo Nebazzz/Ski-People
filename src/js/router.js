03bcc51d4b285fc97f8123d73c607ba3110078aa
Nebazzz <johnpechkin@gmail.com>
2025-04-06 12:46:07 +0300

    router add + slider

---
+++ src/js/router.js
@@ -0,0 +1,23 @@
+import Navigo from "navigo";
+import { header } from "../components/header";
+import { footer } from "../components/footer";
+
+const router = new Navigo('/', { linksSelector: 'a[href^="/"]' });
+
+export const initRouter = () => {
+  router
+    .on('/', () => {
+      document.body.append(
+        header(),
+        footer()
+      );
+    })
+    .on('/favorite', () => {
+      console.log('FAVORITE');
+    })
+    .notFound(() => {
+      console.log('ERROR 404');
+    })
+
+    router.resolve();
+}
