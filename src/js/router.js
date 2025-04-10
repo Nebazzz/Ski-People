31b3995246d588952302eb1198b4cddadce14b1c
Nebazzz <johnpechkin@gmail.com>
2025-04-10 09:25:43 +0300

    components done

--- src/js/router.js
+++ src/js/router.js
@@ -11,31 +11,58 @@ import { footer } from "../components/footer";
 const router = new Navigo('/', { linksSelector: 'a[href^="/"]' });
 
 export const initRouter = () => {
+  const headerElement = header();
+  const footerElement = footer();
+  
+  document.body.append(headerElement);
+  document.body.append(footerElement);
+
   router
     .on('/', () => {
       const main = document.createElement('main');
-
       main.append(
         catalog(),
         breadcrumb(),
         product(),
-        goods(),
-        cart(),
-        order(),
       );
-
-      document.body.append(
-        header(),
-        main,
-        footer()
-      );
+      
+      const oldMain = document.querySelector('main');
+      if (oldMain) oldMain.remove();
+      
+      footerElement.before(main);
+      
+      return main;
     })
     .on('/favorite', () => {
-      console.log('FAVORITE');
+      const main = document.createElement('main');
+      main.append(
+        breadcrumb(),
+        goods(),
+      );
+      
+      const oldMain = document.querySelector('main');
+      if (oldMain) oldMain.remove();
+      
+      footerElement.before(main);
+      
+      return main;
+    })
+    .on('/cart', () => {
+      const main = document.createElement('main');
+      main.append(
+        cart(),
+      );
+      
+      const oldMain = document.querySelector('main');
+      if (oldMain) oldMain.remove();
+      
+      footerElement.before(main);
+      
+      return main;
     })
     .notFound(() => {
       console.log('ERROR 404');
-    })
+    });
 
     router.resolve();
 }
